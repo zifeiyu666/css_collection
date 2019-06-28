4caf9fdd8ca784932ff3498d558351e02198d9ee
yxp <yuxinpeng@haier.com>
2019-06-28 17:29:58 +0800

    feat: 添加mixin

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'assets'),
+    compress: true,
+    port: 9000
+  }
+}
